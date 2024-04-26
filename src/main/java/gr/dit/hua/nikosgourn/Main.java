d3e621b758e61c13d181cd36d728aad1e72d6ae9
NikosGour <nikosgournak@gmail.com>
2024-04-26 15:57:37 +0300

    docs: add comments

--- src/main/java/gr/dit/hua/nikosgourn/Main.java
+++ src/main/java/gr/dit/hua/nikosgourn/Main.java
@@ -11,15 +11,31 @@ public class Main {
 		System.out.println(Arrays.toString(recommend_two_movies(MOVIE_DURATIONS , FLIGHT_DURATION)));
 	}
 	
+	/**
+	 * Given a list of movie duration, this function return a pair of indexes that represent the pair of movies , with the longest combined
+	 * duration, constrained but the flight duration - 30
+	 *
+	 * @param movie_durations a list of movie durations to pick a pair from
+	 * @param flight_duration the duration of the flight which is the constraint of the problem
+	 * @return a pair of indexes of the `movie_duration` param, which have the biggest sum of duration
+	 */
 	private static int[] recommend_two_movies(int[] movie_durations , int flight_duration) {
-		int[] result;
+		int[] return_value;
 		List<int[]> pairs_found = new ArrayList<>();
 		final int MAX_ALLOWED = flight_duration - 30;
 		
+		// Iterate over the movie array
+		// we do i -> 0..length-1 and j -> i+1..length for optimization
+		// there is no reason for j to be lower than i because those pairs have already
+		// been checked. Also, j has no reason to start from cause we don't want to recommend
+		// a user to watch the same movie twice.
+		// Also, i has no reason to go up to the length of the array as all the
+		// pairs of the last item have been checked by the previous iterations
 		for (int i = 0; i < movie_durations.length - 1; i++) {
+			// Initializing movie_a here, for optimization, no need to initialize inside j loop
 			int movie_a = movie_durations[i];
 			for (int j = i + 1; j < movie_durations.length; j++) {
-				//				if(i == j) { continue;}
+				// Checking if pair of movies satisfy constraint and add them to candidates
 				int movie_b = movie_durations[j];
 				if (movie_a + movie_b <= MAX_ALLOWED) {
 					pairs_found.add(new int[]{ i , j });
@@ -27,6 +43,7 @@ public class Main {
 			}
 		}
 		
+		// Finding the pair with the longest combined duration
 		int max = 0;
 		int max_index = 0;
 		for (int i = 0; i < pairs_found.size(); i++) {
@@ -37,7 +54,7 @@ public class Main {
 				max_index = i;
 			}
 		}
-		result = pairs_found.get(max_index);
-		return result;
+		return_value = pairs_found.get(max_index);
+		return return_value;
 	}
 }
